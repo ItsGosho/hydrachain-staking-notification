3577f36cd241945c9cf4ecffa240ba26a43c4224
itsgosho <itsgosho2@gmail.com>
2023-02-15 02:38:22 +0200

    Final full default message for the mining sms. from & to to come from args

--- test-twilio.py
+++ test-twilio.py
@@ -9,12 +9,12 @@ client = Client(hydrachain_arguments.get_twilio_account_sid(), hydrachain_argume
 1. Option to format the decimal places of the SMS.
 """
 
-format_1 = "5.88 Hydra Mined."
+format_1 = "Hydra Mined:\n5.88123415.\n10-February-2023 21:22\nH8Rw6bdg8zYgQ8YYm5K7zkK6EytetYsAuY"
 
 message = client.messages.create(
-  body="5.88123415 Hydra Mined",
-  from_="+16233043804",
-  to="+359896997166"
+  body=format_1,
+  from_=hydrachain_arguments.get_twilio_from_number(),
+  to=hydrachain_arguments.get_sms_to_number()
 )
 
 print(message.sid)
