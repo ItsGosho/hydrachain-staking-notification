b43c221010fe94de270de6c047af41778170afca
itsgosho <itsgosho2@gmail.com>
2023-03-04 03:59:29 +0200

    Changed to use the Hydrachain Explorer Library for making requests to the explroer

--- explorer/explorer_reader.py
+++ explorer/explorer_reader.py
@@ -1,38 +1,20 @@
 import logging
-import requests
+from hydrachain_explorer_requester.explorer_requester import ExplorerRequester
 
 logger = logging.getLogger(__name__)
 
-def request_address_transaction_ids(address, page=0, page_size=20):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
-    url = f"https://explorer.hydrachain.org/7001/address/{address}/txs?page={page}&pageSize={page_size}"
-    response = requests.get(url=url, headers=headers)
-    logger.debug(f"GET {url} responded with status code {response.status_code}, body {response.content}")
-    return response.json()
-
-
-def request_transactions(transaction_ids):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
-    url = f"https://explorer.hydrachain.org/7001/txs/{','.join(transaction_ids)}"
-    response = requests.get(url=url, headers=headers)
-    logger.debug(f"GET {url} responded with status code {response.status_code}, body {response.content}")
-    return response.json()
-
 
 def request_transactions_created_after(address, created_after):
+    explorer_requester = ExplorerRequester()
+
     transactions_created_after = []
+    for transactionId in explorer_requester.get_address_transactions_iterator(address):
+        transaction = explorer_requester.get_transaction(transactionId)
 
-    while True:
-        address_transaction_ids = request_address_transaction_ids(address)["transactions"]
-        transactions = request_transactions(address_transaction_ids)
-
-        for transaction in transactions:
-            if (transaction["timestamp"] >= created_after.timestamp()):
-                transactions_created_after.append(transaction)
-            else:
-                return transactions_created_after
+        if (transaction["timestamp"] >= created_after.timestamp()):
+            transactions_created_after.append(transaction)
+        else:
+            return transactions_created_after
 
 
 def request_mined_transactions_created_after(address, created_after):
@@ -42,19 +24,3 @@ def request_mined_transactions_created_after(address, created_after):
     filteredTransactions = list(
         filter(lambda transaction: transaction["isCoinstake"] == True, transactions_created_after))
     return filteredTransactions
-
-# address = "XXX"
-# mined_transactions_until = request_mined_transactions_after(address, datetime.datetime.now() - datetime.timedelta(days =10))
-# print(json.dumps(mined_transactions_until, indent = 4))
-
-# def requestAllAddressTransactions(address):
-#
-#     addressTransactionIds = requestAllAddressTransactionIds(address)["transactions"]
-#     allAddressTransactions = []
-#     atTime = 20
-#     for j in range(0, len(addressTransactionIds), atTime):
-#         transactionIds = addressTransactionIds[j: j + atTime]
-#         transactions = requestTransactions(transactionIds)
-#         allAddressTransactions.extend(transactions)
-#
-#     return allAddressTransactions
